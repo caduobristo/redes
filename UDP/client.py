250ed44e2ae2a2ae5af3eb4c43e7529348f41902
carlosobristo <carlos.eduardo@policorp.com.br>
2025-04-25 10:34:58 -0300

    Adicição de cabeçalhos ao pacote

--- UDP/client.py
+++ UDP/client.py
@@ -1,13 +1,14 @@
 import socket
 import os
+import struct
 
 SERVER_IP = '127.0.0.1' 
 SERVER_PORT = 5005
 
-file = input("Digite o caminho do arquivo: ").strip()
+file = input("[Cliente]Digite o caminho do arquivo: ").strip()
 
 if not os.path.exists(file):
-    print("Arquivo não encontrado.")
+    print("[Cliente]Arquivo não encontrado.")
     exit()
 
 file_name = os.path.basename(file)
@@ -15,13 +16,20 @@ file_name = os.path.basename(file)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(file_name.encode(), (SERVER_IP, SERVER_PORT))
 
+seq_num = 0
 with open(file, 'rb') as f:
     while True:
-        dados = f.read(4096)
-        if not dados:
+        data = f.read(4092)
+        if not data:
             break
-        sock.sendto(dados, (SERVER_IP, SERVER_PORT))
+        sock.sendto(data, (SERVER_IP, SERVER_PORT))
+
+        header = struct.pack('!I', seq_num)
+        package = header + data
+        sock.sendto(package, (SERVER_IP, SERVER_PORT))
+        print(f"[Cliente] Pacote {seq_num} enviado ({len(data)} bytes)")
+        seq_num += 1
 
 # Envia mensagem para finalizar o server
 sock.sendto(b'EOF', (SERVER_IP, SERVER_PORT))
-print("Arquivo enviado com sucesso.")
+print("[Cliente]Arquivo enviado com sucesso.")
--- UDP/server.py
+++ UDP/server.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 IP = '0.0.0.0'  # Escuta em todas as interfaces
 PORT = 5005    # PORT maior que 1024
@@ -6,19 +7,39 @@ PORT = 5005    # PORT maior que 1024
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((IP, PORT))
 
-print(f"Aguardando dados na PORT {PORT}...")
+print(f"[Servidor]Aguardando dados na PORT {PORT}...")
 
 file, adress = sock.recvfrom(4096)
 file = file.decode()
 
-print(f"Recebendo arquivo: {file}")
+print(f"[Servidor]Recebendo arquivo: {file}")
+
+received_data = {}
+
+while True:
+    package, _ = sock.recvfrom(4096)
+    if package == b'EOF':
+        print("[Servidor]Recebido EOF. Fim da transmissão.")
+        break
+
+    if len(package) < 4:
+        print(f"[Servidor]Pacote inválido recebido {len(package)}")
+        continue
+
+    try:
+        seq_num = struct.unpack('!I', package[:4])[0]
+        data = package[4:]
+
+        received_data[seq_num] = data
+        print(f"[Servidor] Pacote {seq_num} recebido ({len(data)} bytes)")
+    except struct.error:
+        print("[Servidor] Erro ao desempacotar número de sequência. Pacote ignorado.")
+
 
 with open(f"received_{file}" , 'wb') as f:
-    while True:
-        dados, _ = sock.recvfrom(4096)
-        if dados == b'EOF':
-            print("Recebido EOF. Fim da transmissão.")
-            break
-        f.write(dados)
+    for data in sorted(received_data.keys()):
+        f.write(received_data[data])
+        print(f"[Servidor]Gravando pacote {data}")
 
-print(f"Arquivo salvo como received_{file}")
+
+print(f"[Servidor]Arquivo salvo como received_{file}")
